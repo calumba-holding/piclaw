e65f9cb2a7ebc5c779907020b07568a2d6cecabe
Rui Carmo <rui.carmo@gmail.com>
2026-03-03 23:53:51 +0000

    Bump version to 0.2.13

--- piclaw/dist/extensions/index.js
+++ piclaw/dist/extensions/index.js
@@ -2,9 +2,11 @@ import { fileAttachments } from "./file-attachments.js";
 import { messageSearch } from "./message-search.js";
 import { modelControl } from "./model-control.js";
 import { scheduledTasks } from "./scheduled-tasks.js";
+import { workspaceSearch } from "./workspace-search.js";
 export const builtinExtensionFactories = [
     fileAttachments,
     messageSearch,
     modelControl,
     scheduledTasks,
+    workspaceSearch,
 ];
